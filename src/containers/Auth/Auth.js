95763c5f4e96562138a46af36fe2887d52f13493
Roman Zakrevskyi <romul3003@gmail.com>
2019-06-01 18:06:03 +0300

    made form Input component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
+import Input from "../../components/UI/Input/Input"
 
 class Auth extends Component {
     loginHandler = () => {
@@ -22,8 +23,13 @@ class Auth extends Component {
                     <h1>Авторизация</h1>
 
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <input type="text"/>
-                        <input type="text"/>
+                        <Input
+                            label="Email"
+                        />
+                        <Input
+                            label="Пароль"
+                            errorMessage={'TEST'}
+                        />
 
                         <Button
                             type="success"

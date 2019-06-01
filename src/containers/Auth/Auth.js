226b6d72f9def97e78f7f50fb2683b933456bf21
Roman Zakrevskyi <romul3003@gmail.com>
2019-06-01 19:57:12 +0300

    made form validation part 2

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from "../../components/UI/Input/Input"
+import is from 'is_js'
 
 class Auth extends Component {
     state = {
@@ -45,8 +46,42 @@ class Auth extends Component {
         event.preventDefault()
     }
 
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+
+        if (validation.email) {
+            isValid = is.email(value) && isValid
+        }
+
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
+
     onChangeHandler = (event, controlName) => {
-        console.log(`${controlName}: `, event.target.value)
+        // console.log(`${controlName}: `, event.target.value)
+
+        const formControls = {...this.state.formControls}
+        const control = { ...formControls[controlName] }
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        this.setState({
+            formControls
+        })
     }
 
     renderInputs() {

8c629258b84198b99a4446b72af63224c72ba179
Roman Zakrevskyi <romul3003@gmail.com>
2019-06-01 18:48:12 +0300

    made form validation part 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,35 @@ import Button from '../../components/UI/Button/Button'
 import Input from "../../components/UI/Input/Input"
 
 class Auth extends Component {
+    state = {
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
     loginHandler = () => {
 
     }
@@ -16,6 +45,29 @@ class Auth extends Component {
         event.preventDefault()
     }
 
+    onChangeHandler = (event, controlName) => {
+        console.log(`${controlName}: `, event.target.value)
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!control.validation}
+                    errorMessage={control.errorBoundary}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+
     render() {
         return (
             <div className={classes.Auth}>
@@ -23,13 +75,7 @@ class Auth extends Component {
                     <h1>Авторизация</h1>
 
                     <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <Input
-                            label="Email"
-                        />
-                        <Input
-                            label="Пароль"
-                            errorMessage={'TEST'}
-                        />
+                        { this.renderInputs() }
 
                         <Button
                             type="success"

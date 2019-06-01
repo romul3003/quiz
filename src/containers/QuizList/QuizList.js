cefbde07e84df5a60ad1253f2bf9358c618c79f0
Roman Zakrevskyi <romul3003@gmail.com>
2019-06-01 17:12:55 +0300

    made Link button

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import classes from './QuizList.module.css'
+import {NavLink} from 'react-router-dom'
 
 class QuizList extends Component {
+
+    renderQuizes() {
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <li
+                    key={index}
+                >
+                    <NavLink to={'/quiz/' + quiz}>
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz List</h1>
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
             </div>
-        );
+        )
     }
 }
 
-export default QuizList;
+export default QuizList

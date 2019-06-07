02962e2e42aa43f20b993824dd339b4cfc43f91e
Roman Zakrevskyi <romul3003@gmail.com>
2019-06-07 14:30:57 +0300

    made Select component in making tests form

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -4,6 +4,7 @@ import Button from "../../components/UI/Button/Button"
 import {createControl} from '../../form/formFramework'
 import Input from "../../components/UI/Input/Input"
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+import Select from "../../components/UI/Select/Select";
 
 function createOptionControl(number) {
     return createControl({
@@ -30,6 +31,7 @@ export default class QuizCreator extends Component {
 
     state = {
         quiz: [],
+        rightAnswerId: 1,
         formControls: createFormControls()
     }
 
@@ -70,7 +72,25 @@ export default class QuizCreator extends Component {
         })
     }
 
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        })
+    }
+
     render() {
+        const select = <Select
+            label="Выберете правильный ответ"
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4}
+            ]}
+        />
+
         return (
             <div className={classes.QuizCreator}>
                 <div>
@@ -80,7 +100,7 @@ export default class QuizCreator extends Component {
 
                         {this.renderControls()}
 
-                        <select></select>
+                        {select}
 
                         <Button
                             type="primary"

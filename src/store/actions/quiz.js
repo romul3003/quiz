b81408f64e0f9dbcfd027fad729b03b818e86e6c
Roman Zakrevskyi <romul3003@gmail.com>
2019-06-15 17:17:27 +0300

    made test page with redux p1

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -1,5 +1,10 @@
 import axios from '../../axios/axios-quiz'
-import {FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS} from './actionTypes'
+import {
+	FETCH_QUIZ_SUCCESS,
+	FETCH_QUIZES_ERROR,
+	FETCH_QUIZES_START,
+	FETCH_QUIZES_SUCCESS
+} from './actionTypes'
 
 export function fetchQuizes() {
 	return async dispatch => {
@@ -21,6 +26,28 @@ export function fetchQuizes() {
 			dispatch(fetchQuizesError(e))
 		}
 	}
+}
+
+export function fetchQuizById(quizId) {
+    return async dispatch => {
+    	dispatch(fetchQuizesStart())
+
+			try {
+				const response = await axios.get(`/quizes/${quizId}.json`)
+				const quiz = response.data
+
+				dispatch(fetchQuizSuccess(quiz))
+			} catch (e) {
+				dispatch(fetchQuizesError(e))
+			}
+		}
+}
+
+export function fetchQuizSuccess(quiz) {
+    return {
+    	type: FETCH_QUIZ_SUCCESS,
+    	quiz
+		}
 }
 
 export function fetchQuizesStart() {

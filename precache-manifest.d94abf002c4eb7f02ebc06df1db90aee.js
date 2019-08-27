2eff95728d95d3c28e1aa83b7b0e0ed7bc43d70e
Roman Zakrevskyi <romul3003@gmail.com>
2019-08-27 17:07:56 +0300

    Updates

---
+++ precache-manifest.d94abf002c4eb7f02ebc06df1db90aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  },
+  {
+    "revision": "d4e46b16b48dcd31f53c",
+    "url": "/quiz/static/js/main.35a5e735.chunk.js"
+  },
+  {
+    "revision": "cfa63ebbf73e22fecbe9",
+    "url": "/quiz/static/js/2.4d523d73.chunk.js"
+  },
+  {
+    "revision": "d4e46b16b48dcd31f53c",
+    "url": "/quiz/static/css/main.e9732818.chunk.css"
+  },
+  {
+    "revision": "6225ec24b33121885a0c3a2f1f4dea00",
+    "url": "/quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quiz/precache-manifest.665b7fdbc6d2beb4d5779b16376ebd55.js"
+  "/quiz/precache-manifest.d94abf002c4eb7f02ebc06df1db90aee.js"
 );
 
 workbox.clientsClaim();

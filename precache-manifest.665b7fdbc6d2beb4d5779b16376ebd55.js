193ad69dcb65974cb2a3ce85d325629ef6e1ee46
Roman Zakrevskyi <romul3003@gmail.com>
2019-08-27 14:19:02 +0300

    Updates

---
+++ precache-manifest.665b7fdbc6d2beb4d5779b16376ebd55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  },
+  {
+    "revision": "9d06d17b92367f687ccb",
+    "url": "/quiz/static/js/main.ec203bd7.chunk.js"
+  },
+  {
+    "revision": "81579c791e3e11e5e808",
+    "url": "/quiz/static/js/2.ab83aa6d.chunk.js"
+  },
+  {
+    "revision": "9d06d17b92367f687ccb",
+    "url": "/quiz/static/css/main.e9732818.chunk.css"
+  },
+  {
+    "revision": "6806ac0773ac6b70028671c08738d0ee",
+    "url": "/quiz/index.html"
+  }
+];
